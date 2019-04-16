cf345296ecd5c25bd50a643384e4983fe0c02fc1
jeshan <j@jeshan.co>
2019-04-16 13:55:28 +0400

    rm debug logs

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ class Aws(Resolver):
     def resolve(self):
         # self.argument, self.stack
         arg_list = self.argument.split('::')
-        self.logger.info(arg_list)
-        self.logger.info(len(arg_list))
         service = arg_list[0]
         operation = arg_list[1]
         expression = None
@@ -26,14 +24,11 @@ class Aws(Resolver):
         if len(arg_list) > 2 and arg_list[2]:
             args = arg_list[2]
             parsed_args = literal_eval("{" + args + "}")
-            self.logger.info(parsed_args)
         if len(arg_list) > 3 and arg_list[3]:
             expression = arg_list[3]
         # noinspection PyStatementEffect
         self.stack.template  # just to initialise the connection manager
         result = self.stack.connection_manager.call(service, operation, parsed_args)
-        self.logger.info(result)
         if expression:
             result = jmespath.search(expression, result)
-            self.logger.info(result)
         return str(result)

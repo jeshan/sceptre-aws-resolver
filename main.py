fc6c69dfd5253b244b627584db87e9ae957f8b88
jeshan <j@jeshan.co>
2019-04-21 02:05:15 +0400

    add optional region

--- main.py
+++ main.py
@@ -20,15 +20,18 @@ class Aws(Resolver):
         service = arg_list[0]
         operation = arg_list[1]
         expression = None
+        region = None
         parsed_args = {}
         if len(arg_list) > 2 and arg_list[2]:
             args = arg_list[2]
             parsed_args = literal_eval("{" + args + "}")
         if len(arg_list) > 3 and arg_list[3]:
             expression = arg_list[3]
+        if len(arg_list) > 4 and arg_list[4]:
+            region = arg_list[4]
         # noinspection PyStatementEffect
         self.stack.template  # just to initialise the connection manager
-        result = self.stack.connection_manager.call(service, operation, parsed_args)
+        result = self.stack.connection_manager.call(service, operation, parsed_args, region=region)
         if expression:
             result = jmespath.search(expression, result)
         return str(result)
